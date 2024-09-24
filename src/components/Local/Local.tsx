92cfb225de9a592e3a24f681f6541ffeade4d988
nguyenquy42 <nguyenquy26012014@gmail.com>
2024-09-25 00:11:27 +0700

    edit url images

--- src/components/Local/Local.tsx
+++ src/components/Local/Local.tsx
@@ -9,7 +9,7 @@ const Local = () => {
         </div>
         <div className="local_item">
           <h2 className="local_heading-local">Dĩ An</h2>
-          <img className="local_img" src="/public/assets/images/gallery/gallery35.jpg" alt="gallary 01" />
+          <img className="local_img" src="/assets/images/gallery/gallery35.jpg" alt="gallary 01" />
         </div>
         <div className="local_item">
           <p className="local_item-text">số 99, Đường số 9, Dĩ An, Bình Dương</p>
